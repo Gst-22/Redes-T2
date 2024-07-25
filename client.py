dfea3b5c3ae05bf9fc5c1f57c6879be958e27bdc
Guilherme <123751804+Gst-22@users.noreply.github.com>
2024-07-25 02:08:56 -0300

    Teste dar cartas

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 import config as config
 import ast
+import gameplay as game
 import network as net
 from network import Message
 
@@ -15,15 +16,17 @@ rcv_skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 rcv_skt.bind((UDP_IP, RCV_PORT))
 
 if machine_number == 1: #Maquina 1 inicia o jogo
+    carteado = game.sorteiaMaos(4)
+    for i in range(2):
+        message = Message(0, machine_number, i+2, str(carteado[i+2]), 0)
+        enviado = net.ringMessage(message, machine_number, UDP_IP, SND_PORT, rcv_skt)
+        if enviado:
+            print("Enviado para " + str(i+2))
+        else:
+            print("Falha ao enviar.")
+            exit
     
-    text = input("Digite Mensagem\n")
-    message = Message(0, machine_number, int(input("Digite Destinatario:")), text, 0)
-    enviado = net.ringMessage(message, machine_number, UDP_IP, SND_PORT, rcv_skt)
-    if enviado:
-        net.passToken(UDP_IP, SND_PORT)
-    else:
-        print("Falha ao enviar.")
-        exit
+    net.passToken(UDP_IP, SND_PORT)
 
 while True: #espero mensagem, envio confirmação.
     data, addr = rcv_skt.recvfrom(1024)
