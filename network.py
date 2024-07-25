6b11e823bf515aafb25e5598ac48e14cafa87e06
Guilherme <123751804+Gst-22@users.noreply.github.com>
2024-07-25 01:30:29 -0300

    Funções funcionando

--- network.py
+++ network.py
@@ -1,4 +1,5 @@
 import socket
+import ast
 
 class Message:
     def __init__(self, token, origem, destino, msg, recebido):
@@ -17,3 +18,25 @@ def messageTo(message, UPD_IP, SND_PORT):
     send.sendto(str(message).encode("utf-8"), (UPD_IP, SND_PORT))
 
     send.close()
+
+def ringMessage(message, machine_number, UPD_IP, SND_PORT, RCV_SKT):
+
+    messageTo(message, UPD_IP, SND_PORT)
+    #Envia mensagem inicial
+    
+    #Espera confimação de recebimento.
+    while True:
+        data, addr = RCV_SKT.recvfrom(1024)
+        if data:
+            message = ast.literal_eval(data.decode())
+            if message["origem"] == machine_number and message["recebido"] == 1:
+                print("Sucesso!")
+                return 1
+            else:
+                print("Falha!")
+                return 0
+
+def passToken(UPD_IP, SND_PORT):
+    token = Message(1, 0, 0, 0, 0)
+    messageTo(token, UPD_IP, SND_PORT)    
+
